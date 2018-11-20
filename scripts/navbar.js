4d84f69ac42094c02c291867bd5bda4a31bc21dc
Alex Puhl <apuhlswimmer@hotmail.com>
2018-11-19 22:55:32 -0700

    got the navbar working, cleaned up the styling

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,13 +1,9 @@
 'use strict';
 $(document).ready(function () {
-
-    const toggleNav = () => {
-        
-    }
-  let mainNav=$('#main-nav');
-  let navbarToggle=$('#navbar-toggle');
-  mainNav.hide();
-
+  let mainNav = $('#js-main-nav');
+  let navbarToggle = $('#js-navbar-toggle');
+  // mainNav.hide();
+  
   navbarToggle.click(function(){
     if(navbarToggle.hasClass('active')){
       mainNav.hide();
@@ -19,41 +15,30 @@ $(document).ready(function () {
   });
 
   // Select all links with hashes
-//   $('a[href*="#"]')
-//     // Remove links that don't actually link to anything
-//     .not('[href="#"]')
-//     .not('[href="#0"]')
-//     .click(function(event) {
-//       // On-page links
-//       if (
-//         location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '')
-//         &&
-//         location.hostname === this.hostname
-//       ) {
-//         // Figure out element to scroll to
-//         var target = $(this.hash);
-//         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-//         // Does a scroll target exist?
-//         if (target.length) {
-//           // Only prevent default if animation is actually gonna happen
-//           event.preventDefault();
-//           mainNav.hide();
-//           navbarToggle.removeClass('active');
-//           $('html, body').animate({
-//             scrollTop: target.offset().top - 80
-//           }, 1000, function() {
-//             // Callback after animation
-//             // Must change focus!
-//             var $target = $(target);
-//             $target.focus();
-//             if ($target.is(":focus")) { // Checking if the target was focused
-//               return false;
-//             } else {
-//               $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-//               $target.focus(); // Set focus again
-//             }
-//           });
-//         }
-//       }
-//     });
+  $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '')
+        &&
+        location.hostname === this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          mainNav.hide();
+          navbarToggle.removeClass('active');
+          $('html, body').animate({
+            scrollTop: target.offset().top - 60
+          }, 750);
+        }
+      }
+    });
 });
